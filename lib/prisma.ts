1c70b2489cfe03aafaf4fadafe980f6600a2819c
AliTanha <tmt_ali@yahoo.com>
2025-07-27 20:15:15 +0330

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,12 @@
-import { PrismaClient } from '@/src/generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  // @ts-ignore
+  var prisma: PrismaClient | undefined;
 }
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
