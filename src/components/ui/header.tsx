ce4eb731242804710de151e79f76d479de46a5c1
AliTanha <tmt_ali@yahoo.com>
2025-08-01 00:24:59 +0330

    header update

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "../ThemeToggle";
 
 const Header = () => {
     return(
-        <header className={`fixed w-full flex justify-between p-4 h-16 top-0 ${theme}`}>
+        <header className={`fixed w-full flex justify-between p-4 h-16 top-0 z-10 ${theme}`}>
         <BackMenu/>
         <ThemeToggle />
       </header>
