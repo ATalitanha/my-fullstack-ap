f891019cfb2ffc27554e41b6b49b3b9508b58297
AliTanha <tmt_ali@yahoo.com>
2025-10-03 23:20:32 +0330

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Calculator() {
           className="inline-flex justify-center items-center py-2 px-5 m-5 rounded-xs bg-gray-500 hover:bg-gray-300 text-white"
           about="cal"
         >
-          لیسا کار ها
+          لیست کار ها
         </Link>
         <Link
           href="/notes"
